97c2773df2ae89ab113872b812e3154ec4510d69
Stanley <snicholl@gmail.com>
2017-11-10 11:39:29 -0800

    fixed knexfile

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 const localhostURL = 'http://localhost:3000/movies'
-const herokuURL = 'https://paul-walker-glory.herokuapp.com/movies'
+const herokuURL = 'https://paul-walker-superstar.herokuapp.com/movies'
 const baseURL = window.location.href.includes('127.0.0.1') ? localhostURL : herokuURL
 
 loadList()
-
-const banned = localStorage.getItem('pwBanned')
-
-if(banned === 'true'){
-  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-}
